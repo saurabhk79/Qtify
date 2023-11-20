759df8dce9bb7062702c905bcc8868724bdb34f0
Sauby <imsauby@gmail.com>
2023-11-20 21:05:07 +0530

    added searchbar functionality

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [newAlbumsData, setNewAlbumsData] = useState([]);
   const [songsData, setSongsData] = useState([]);
   const [filteredSongs, setFilteredSongs] = useState([]);
+  const [allAlbumsData, setAllAlbumsData] = useState([]);
 
   const [value, setValue] = useState(0);
 
@@ -77,9 +78,15 @@ function App() {
     generateAllSongsData();
   }, []);
 
+  useEffect(() => {
+    if (topAlbumsData && topAlbumsData) {
+      setAllAlbumsData([...topAlbumsData, ...newAlbumsData]);
+    }
+  }, [topAlbumsData, newAlbumsData]);
+
   return (
     <div>
-      <Navbar />
+      <Navbar allAlbumsData={allAlbumsData} />
       <Hero />
       <div>
         <Section data={topAlbumsData} title={"Top Albums"} type={"album"} />
--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,13 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./search.module.css";
+import { Autocomplete, TextField, Paper } from "@mui/material";
+import Options from "./Options";
 
-function Searchbar() {
+function Searchbar({ allAlbumsData }) {
+  const [val, setVal] = useState("");
+  const [filteredData, setFilteredData] = useState([]);
+  const [timeOut, setTimeOut] = useState();
+
+  const SONGS_LIST = allAlbumsData.map((song) => song.songs).flat();
+
+  const findSongs = () => {
+    let songs = SONGS_LIST.filter((song) => {
+      return (
+        song.title.toLowerCase().includes(val) ||
+        song.artists.join(" ").toLowerCase().includes(val)
+      );
+    });
+    !val ? setFilteredData([]) : setFilteredData(songs);
+  };
+
+  const debounceSearch = (value, t) => {
+    if (timeOut) {
+      clearTimeout(timeOut);
+    }
+    let time = setTimeout(() => {
+      setVal(value);
+    }, t);
+    setTimeOut(time);
+  };
+
+  const inputChangeHandler = (e) => {
+    debounceSearch(e.target.value, 300);
+  };
+
+  useEffect(() => {
+    findSongs();
+  }, [val]);
+
   return (
     <div className={styles.navSearchBox}>
-      <input
-        type="text"
-        placeholder="Search an album of your choice"
+      <Autocomplete
+        freeSolo
+        id="free-solo-2-demo"
+        disableClearable
+        ListboxProps={{ className: "autoComplete" }}
+        PaperComponent={({ children }) => (
+          <Paper className={styles.paper}>{children}</Paper>
+        )}
+        options={filteredData.length ? filteredData : allAlbumsData}
+        getOptionLabel={(option) => option.title}
+        renderOption={(props, option) => {
+          return (
+            <Options
+              key={option.id}
+              album={option.title}
+              follows={option.follows}
+              likes={option.likes}
+              url={option.image}
+              songs={option.songs}
+              artistsNames={option.artists}
+            />
+          );
+        }}
         className={styles.textbox}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            placeholder="Search input"
+            focused
+            onChange={inputChangeHandler}
+          />
+        )}
       />
       <hr />
       <img src="Search icon.svg" alt="search" className={styles.searchIcon} />
